5a3f73b86d4cbba8f16f959a5a9e2225ff3c84c3
Wataru Ishida <ishida.wataru@lab.ntt.co.jp>
2017-06-14 06:41:38 +0000

    fix compilation error on platforms other than amd64,linux
    Signed-off-by: Wataru Ishida <ishida.wataru@lab.ntt.co.jp>

--- sctp.go
+++ sctp.go
@@ -1,12 +1,9 @@
-// +build linux,amd64
-
 package sctp
 
 import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
-	"io"
 	"net"
 	"strconv"
 	"strings"
@@ -199,34 +196,6 @@ func htons(h uint16) uint16 {
 
 var ntohs = htons
 
-func setsockopt(fd int, optname, optval, optlen uintptr) (uintptr, uintptr, error) {
-	r0, r1, errno := syscall.Syscall6(syscall.SYS_SETSOCKOPT,
-		uintptr(fd),
-		SOL_SCTP,
-		optname,
-		optval,
-		optlen,
-		0)
-	if errno != 0 {
-		return r0, r1, errno
-	}
-	return r0, r1, nil
-}
-
-func getsockopt(fd int, optname, optval, optlen uintptr) (uintptr, uintptr, error) {
-	r0, r1, errno := syscall.Syscall6(syscall.SYS_GETSOCKOPT,
-		uintptr(fd),
-		SOL_SCTP,
-		optname,
-		optval,
-		optlen,
-		0)
-	if errno != 0 {
-		return r0, r1, errno
-	}
-	return r0, r1, nil
-}
-
 func setNumOstreams(fd, num int) error {
 	param := InitMsg{
 		NumOstreams: uint16(num),
@@ -370,65 +339,9 @@ func (c *SCTPConn) Write(b []byte) (int, error) {
 	return c.SCTPWrite(b, nil)
 }
 
-func (c *SCTPConn) SCTPWrite(b []byte, info *SndRcvInfo) (int, error) {
-	var cbuf []byte
-	if info != nil {
-		cmsgBuf := toBuf(info)
-		hdr := &syscall.Cmsghdr{
-			Len:   uint64(syscall.CmsgSpace(len(cmsgBuf))),
-			Level: syscall.IPPROTO_SCTP,
-			Type:  SCTP_CMSG_SNDRCV,
-		}
-		cbuf = append(toBuf(hdr), cmsgBuf...)
-	}
-	return syscall.SendmsgN(c.fd, b, cbuf, nil, 0)
-}
-
 func (c *SCTPConn) Read(b []byte) (int, error) {
 	n, _, err := c.SCTPRead(b)
 	return n, err
-}
-
-func parseSndRcvInfo(b []byte) (*SndRcvInfo, error) {
-	msgs, err := syscall.ParseSocketControlMessage(b)
-	if err != nil {
-		return nil, err
-	}
-	for _, m := range msgs {
-		if m.Header.Level == syscall.IPPROTO_SCTP {
-			switch m.Header.Type {
-			case SCTP_CMSG_SNDRCV:
-				return (*SndRcvInfo)(unsafe.Pointer(&m.Data[0])), nil
-			}
-		}
-	}
-	return nil, nil
-}
-
-func (c *SCTPConn) SCTPRead(b []byte) (int, *SndRcvInfo, error) {
-	oob := make([]byte, 254)
-	for {
-		n, oobn, recvflags, _, err := syscall.Recvmsg(c.fd, b, oob, 0)
-		if err != nil {
-			return n, nil, err
-		}
-
-		if n == 0 && oobn == 0 {
-			return 0, nil, io.EOF
-		}
-
-		if recvflags&MSG_NOTIFICATION > 0 && c.notificationHandler != nil {
-			if err := c.notificationHandler(b[:n]); err != nil {
-				return 0, nil, err
-			}
-		} else {
-			var info *SndRcvInfo
-			if oobn > 0 {
-				info, err = parseSndRcvInfo(oob[:oobn])
-			}
-			return n, info, err
-		}
-	}
 }
 
 func (c *SCTPConn) SetInitMsg(numOstreams, maxInstreams, maxAttempts, maxInitTimeout int) error {
@@ -618,14 +531,6 @@ func (c *SCTPConn) RemoteAddr() net.Addr {
 	return addr
 }
 
-func (c *SCTPConn) Close() error {
-	info := &SndRcvInfo{
-		Flags: SCTP_EOF,
-	}
-	c.SCTPWrite(nil, info)
-	return syscall.Close(c.fd)
-}
-
 func (c *SCTPConn) PeelOff(id int) (*SCTPConn, error) {
 	type peeloffArg struct {
 		assocId int32
@@ -659,125 +564,12 @@ type SCTPListener struct {
 	m  sync.Mutex
 }
 
-func ListenSCTP(net string, laddr *SCTPAddr) (*SCTPListener, error) {
-	af := syscall.AF_INET
-	switch net {
-	case "sctp":
-		hasv6 := func(addr *SCTPAddr) bool {
-			if addr == nil {
-				return false
-			}
-			for _, ip := range addr.IP {
-				if ip.To4() == nil {
-					return true
-				}
-			}
-			return false
-		}
-		if hasv6(laddr) {
-			af = syscall.AF_INET6
-		}
-	case "sctp4":
-	case "sctp6":
-		af = syscall.AF_INET6
-	default:
-		return nil, fmt.Errorf("invalid net: %s", net)
-	}
-
-	sock, err := syscall.Socket(
-		af,
-		syscall.SOCK_STREAM,
-		syscall.IPPROTO_SCTP,
-	)
-	if err != nil {
-		return nil, err
-	}
-	err = setNumOstreams(sock, SCTP_MAX_STREAM)
-	if err != nil {
-		return nil, err
-	}
-	if laddr != nil && len(laddr.IP) != 0 {
-		err := SCTPBind(sock, laddr, SCTP_BINDX_ADD_ADDR)
-		if err != nil {
-			return nil, err
-		}
-	}
-	err = syscall.Listen(sock, 5)
-	if err != nil {
-		return nil, err
-	}
-	return &SCTPListener{
-		fd: sock,
-	}, nil
-}
-
-func (ln *SCTPListener) Accept() (net.Conn, error) {
-	ln.m.Lock()
-	defer ln.m.Unlock()
-	fd, _, err := syscall.Accept4(ln.fd, 0)
-	return NewSCTPConn(fd, nil), err
-}
-
-func (ln *SCTPListener) Close() error {
-	syscall.Shutdown(ln.fd, syscall.SHUT_RDWR)
-	return syscall.Close(ln.fd)
-}
-
 func (ln *SCTPListener) Addr() net.Addr {
 	laddr, err := sctpGetAddrs(ln.fd, 0, SCTP_GET_LOCAL_ADDRS)
 	if err != nil {
 		return nil
 	}
 	return laddr
-}
-
-func DialSCTP(net string, laddr, raddr *SCTPAddr) (*SCTPConn, error) {
-	af := syscall.AF_INET
-	switch net {
-	case "sctp":
-		hasv6 := func(addr *SCTPAddr) bool {
-			if addr == nil {
-				return false
-			}
-			for _, ip := range addr.IP {
-				if ip.To4() == nil {
-					return true
-				}
-			}
-			return false
-		}
-		if hasv6(laddr) || hasv6(raddr) {
-			af = syscall.AF_INET6
-		}
-	case "sctp4":
-	case "sctp6":
-		af = syscall.AF_INET6
-	default:
-		return nil, fmt.Errorf("invalid net: %s", net)
-	}
-	sock, err := syscall.Socket(
-		af,
-		syscall.SOCK_STREAM,
-		syscall.IPPROTO_SCTP,
-	)
-	if err != nil {
-		return nil, err
-	}
-	err = setNumOstreams(sock, SCTP_MAX_STREAM)
-	if err != nil {
-		return nil, err
-	}
-	if laddr != nil {
-		err := SCTPBind(sock, laddr, SCTP_BINDX_ADD_ADDR)
-		if err != nil {
-			return nil, err
-		}
-	}
-	_, err = SCTPConnect(sock, raddr)
-	if err != nil {
-		return nil, err
-	}
-	return NewSCTPConn(sock, nil), nil
 }
 
 type SCTPSndRcvInfoWrappedConn struct {
