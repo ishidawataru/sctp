b37d4d8935461fc2c21c061cbb37e9b99c3d7589
Evgeniy Makeev <evgeniym@fb.com>
2018-01-31 16:43:50 -0800

    Go SCTP bug fixes: 1) Fix to avoid returning negative read lenth on failed SCTP Reads (it breaks std Go buffered IO) 2) Atomic access to internal SCTP Connection fd & connection close FD cleanup to prevent "double" fd closure which may affect another connection in multithreaded environments.

--- sctp_linux.go
+++ sctp_linux.go
@@ -114,9 +114,12 @@ func (c *SCTPConn) Close() error {
 	return syscall.EBADF
 }
 
-func ListenSCTP(network string, laddr *SCTPAddr) (*SCTPListener, error) {
-	af, ipv6only := favoriteAddrFamily(network, laddr, nil, "listen")
+func ListenSCTP(net string, laddr *SCTPAddr) (*SCTPListener, error) {
+	return ListenSCTPExt(net, laddr, InitMsg{NumOstreams: SCTP_MAX_STREAM})
+}
 
+func ListenSCTPExt(network string, laddr *SCTPAddr, options InitMsg) (*SCTPListener, error) {
+	af, _ := favoriteAddrFamily(network, laddr, nil, "listen")
 	sock, err := syscall.Socket(
 		af,
 		syscall.SOCK_STREAM,
@@ -125,19 +128,13 @@ func ListenSCTP(network string, laddr *SCTPAddr) (*SCTPListener, error) {
 	if err != nil {
 		return nil, err
 	}
-
-	if err = setDefaultSockopts(sock, af, ipv6only); err != nil {
-		syscall.Close(sock)
-		return nil, err
-	}
-
-	err = setNumOstreams(sock, SCTP_MAX_STREAM)
+	err = setInitOpts(sock, options)
 	if err != nil {
 		return nil, err
 	}
 
 	if laddr != nil {
-		//If IP address and/or port was not provided so far, let's use the unspecified IPv4 or IPv6 address
+		// If IP address and/or port was not provided so far, let's use the unspecified IPv4 or IPv6 address
 		if len(laddr.IPAddrs) == 0 {
 			if af == syscall.AF_INET {
 				laddr.IPAddrs = append(laddr.IPAddrs, net.IPAddr{IP: net.IPv4zero})
@@ -169,9 +166,12 @@ func (ln *SCTPListener) Close() error {
 	return syscall.Close(ln.fd)
 }
 
-func DialSCTP(network string, laddr, raddr *SCTPAddr) (*SCTPConn, error) {
-	af, ipv6only := favoriteAddrFamily(network, laddr, nil, "dial")
+func DialSCTP(net string, laddr, raddr *SCTPAddr) (*SCTPConn, error) {
+	return DialSCTPExt(net, laddr, raddr, InitMsg{NumOstreams: SCTP_MAX_STREAM})
+}
 
+func DialSCTPExt(network string, laddr, raddr *SCTPAddr, options InitMsg) (*SCTPConn, error) {
+	af, _ := favoriteAddrFamily(network, laddr, nil, "dial")
 	sock, err := syscall.Socket(
 		af,
 		syscall.SOCK_STREAM,
@@ -180,18 +180,19 @@ func DialSCTP(network string, laddr, raddr *SCTPAddr) (*SCTPConn, error) {
 	if err != nil {
 		return nil, err
 	}
-
-	if err = setDefaultSockopts(sock, af, ipv6only); err != nil {
-		syscall.Close(sock)
-		return nil, err
-	}
-
-	err = setNumOstreams(sock, SCTP_MAX_STREAM)
+	err = setInitOpts(sock, options)
 	if err != nil {
 		return nil, err
 	}
-
 	if laddr != nil {
+		// If IP address and/or port was not provided so far, let's use the unspecified IPv4 or IPv6 address
+		if len(laddr.IPAddrs) == 0 {
+			if af == syscall.AF_INET {
+				laddr.IPAddrs = append(laddr.IPAddrs, net.IPAddr{IP: net.IPv4zero})
+			} else if af == syscall.AF_INET6 {
+				laddr.IPAddrs = append(laddr.IPAddrs, net.IPAddr{IP: net.IPv6zero})
+			}
+		}
 		err := SCTPBind(sock, laddr, SCTP_BINDX_ADD_ADDR)
 		if err != nil {
 			return nil, err
